6c4f4d4be5fd7dca13cd3d936c995c9f22107213
Karen Martirosyan <info@powersoft.tv>
2016-04-30 10:21:25 -0700

    check phpstorm

---
+++ ajax/logout.php
@@ -0,0 +1,20 @@
+<?php
+$empty=false;
+    $data =  base64_decode($_POST['data']);
+    if(!isset($data) || $data==""){
+        $empty=true;
+    }
+    
+    if(!$empty){
+        $tkn=mysqli_real_escape_string($MV,$data);
+        $query="DELETE FROM nf_users_tokens
+                WHERE users_token='".$tkn."'
+        ";
+        @mysqli_query($MV,$query);
+           // $response = $query;
+
+    }
+   //$response = base64_decode($_POST['data']);
+   
+    
+?>
