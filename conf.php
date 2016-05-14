1d4f2da182f172824605fee5af61041c90d7112e
Karen Martirosyan <info@powersoft.tv>
2016-05-14 11:17:12 -0700

    testtttt

--- conf.php
+++ conf.php
@@ -3,7 +3,13 @@
 	$db_user='root';
 	$db_pass='123456';
 	$db_name='nf.dev';
-    
+
+//    $db_host='db603114527.db.1and1.com';
+//    $db_user='dbo603114527';
+//    $db_pass='Power2015!!';
+//    $db_name='db603114527';
+
+
     $cookie_exp = time()+60*20; //20min
     $admin_email="markareno@gmail.com";
 
@@ -11,7 +17,7 @@
 
     $languages=array('en','ru','hy');
     $front_languages=array('en','ru','hy');
-          
+
     $VERSION=1.01;
     
 ?>
