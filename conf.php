4b680d9fb53397fc2fb0e905d6710bce5ebc7b88
Karen Martirosyan <info@powersoft.tv>
2016-05-14 11:18:28 -0700

    testtttt

--- conf.php
+++ conf.php
@@ -1,13 +1,13 @@
 <?php
-    $db_host='localhost';
-	$db_user='root';
-	$db_pass='123456';
-	$db_name='nf.dev';
+//    $db_host='localhost';
+//	$db_user='root';
+//	$db_pass='123456';
+//	$db_name='nf.dev';
 
-//    $db_host='db603114527.db.1and1.com';
-//    $db_user='dbo603114527';
-//    $db_pass='Power2015!!';
-//    $db_name='db603114527';
+    $db_host='db603114527.db.1and1.com';
+    $db_user='dbo603114527';
+    $db_pass='Power2015!!';
+    $db_name='db603114527';
 
 
     $cookie_exp = time()+60*20; //20min
