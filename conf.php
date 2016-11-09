458f96cb03c1d1f88159fae408a84265b30c9183
Karen.M <karen.m@t3leads.com>
2016-11-09 13:46:14 -0800

    sdghsh

--- conf.php
+++ conf.php
@@ -20,9 +20,8 @@
         'en'=>'!!!_English_!!!',
         'ru'=>'!!!_Russian_!!!',
         'hy'=>'!!!_Armenian_!!!',
-
-
         'fr'=>'!!!_French_!!!',
+    	'ka'=>'!!!_Georgian_!!!',
     	'ka'=>'!!!_Georgian_!!!',
 	);
     $front_languages=array('en','ru','hy');
